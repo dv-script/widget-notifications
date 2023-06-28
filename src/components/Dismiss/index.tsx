15fba2a8e42c27b4be1650ac24c2abff294ef3ed
Davi Hasson Castro <davihasson@gmail.com>
2023-06-28 17:23:47 -0300

    feat: new Dismiss component

---
+++ src/components/Dismiss/index.tsx
@@ -0,0 +1,9 @@
+import { Minus } from "lucide-react";
+
+export default function Dismiss() {
+    return (
+        <button className="m-auto">
+            <Minus size="22" className="text-zinc-500"/>
+        </button>
+    )
+}
