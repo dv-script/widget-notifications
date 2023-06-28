2c385a4244cb7faec9976db000de71971e493088
Davi Hasson Castro <davihasson@gmail.com>
2023-06-28 17:23:26 -0300

    fix: fixing Actions component

--- src/components/Actions/index.tsx
+++ src/components/Actions/index.tsx
@@ -4,11 +4,11 @@ export default function Actions() {
     return (
         <>
             <div className="flex gap-2 self-center">
-                <button className="w-8 h-8 rounded flex items-center justify-center bg-zinc-400 hover:bg-zinc-500 dark:bg-zinc-800 hover:bg-zinc-700">
-                    <X className="w-3 h-3 text-zinc-50"/>
+                <button className="w-8 h-8 rounded flex items-center justify-center bg-zinc-400 dark:bg-zinc-800 hover:bg-zinc-700">
+                    <X size="20" className="text-zinc-50"/>
                 </button>
                 <button className="w-8 h-8 rounded flex items-center justify-center bg-violet-500 hover:bg-violet-400">
-                    <Check className="w-3 h-3 text-zinc-50"/>  
+                    <Check size="20" className="text-zinc-50"/>  
                 </button>
             </div>
         </>
