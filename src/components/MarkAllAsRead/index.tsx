d549306e60be76cffd28b50e5244307495cf493d
Davi Hasson Castro <davihasson@gmail.com>
2023-06-28 21:01:21 -0300

    refator: refatoring MarkAllAsRead component

--- src/components/MarkAllAsRead/index.tsx
+++ src/components/MarkAllAsRead/index.tsx
@@ -1,6 +1,9 @@
-export default function MarkAllAsRead() {
+import { MarkAllAsReadProps } from "./type";
+
+export default function MarkAllAsRead({ setAllAsRead }:MarkAllAsReadProps) {
     return (
             <button
+                onClick={() => setAllAsRead(true)} 
                 className="text-violet-500 font-bold text-xs uppercase hover:text-violet-400"
             >
                 Mark all as read
--- src/components/Widget/Widget.tsx
+++ src/components/Widget/Widget.tsx
@@ -1,39 +1,53 @@
+"use client"
+
+import { useState } from "react";
+
 import Feed from "../Feed";
 import Notification from "../Notification";
 import MarkAllAsRead from "../MarkAllAsRead";
 
 export default function Widget() {
+
+    const [showFeed, setShowFeed] = useState(false);
+
     return (
         <div className="w-[448px] rounded overflow-hidden">
 
-            {/* Header */}
             <header className=" bg-zinc-200 dark:bg-zinc-900 py-4 px-6 flex items-center justify-between">
                 <span className="font-bold">Notifications</span>
-                <MarkAllAsRead />
+                <MarkAllAsRead 
+                    setAllAsRead={ (readed:boolean) => setShowFeed(readed) }
+                />
             </header>
 
-            <Feed feedTitle="Recents">
-                <Notification 
-                    hoursAgo={2}
-                    icon="Rocket"
-                    typeNotification="Invitation"
-                    textNotification="You have received an invitation to join the Rocketseat company."
-                />
-                <Notification 
-                    hoursAgo={1}
-                    icon="Chat"
-                    typeNotification="Chat"
-                    textNotification="You have received a message."
-                />
-            </Feed>
-            <Feed feedTitle="Latest">
-                <Notification
-                    hoursAgo={2}
-                    icon="Chat"
-                    typeNotification="Chat"
-                    textNotification="You have received a message."
-                />
-            </Feed>
+            { !showFeed ? (
+                <>
+                    <Feed feedTitle="Recents">
+                        <Notification 
+                            hoursAgo={2}
+                            icon="Rocket"
+                            typeNotification="Invitation"
+                            textNotification="You have received an invitation to join the Rocketseat company."
+                        />
+                        <Notification 
+                            hoursAgo={1}
+                            icon="Chat"
+                            typeNotification="Chat"
+                            textNotification="You have received a message."
+                        />
+                    </Feed>
+                    <Feed feedTitle="Latest">
+                        <Notification
+                            hoursAgo={2}
+                            icon="Chat"
+                            typeNotification="Chat"
+                            textNotification="You have received a message."
+                        />
+                    </Feed>
+                </>
+             ): null}
+
+            
         </div>
     )
 }
