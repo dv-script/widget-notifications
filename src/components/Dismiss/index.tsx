f4e582360d2e6a5c15f44b378a3db1cf9bb7a7bd
Davi Hasson Castro <davihasson@gmail.com>
2023-06-30 14:15:39 -0300

    style: changing Dismiss style

--- src/components/Dismiss/index.tsx
+++ src/components/Dismiss/index.tsx
@@ -3,7 +3,7 @@ import { DismissProps } from "./type";
 
 export default function Dismiss({ clicked }: DismissProps) {
   return (
-    <button className="m-auto" onClick={clicked}>
+    <button className="w-8 h-8 rounded flex items-center justify-center bg-zinc-400 dark:bg-zinc-800 hover:bg-zinc-700" onClick={clicked}>
       <Minus size="22" className="text-zinc-500" />
     </button>
   );
